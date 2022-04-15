b7d1aa115430dbb589aab746ea51856ef354a469
SOLEDAD <SOLEDAD@DESKTOP-J9P61QP>
2022-04-15 11:51:31 -0300

    inicializacion de trabajo

---
+++ TP2_Grupo11/src/main/java/fi/unju/edu/ar/aplicacion/Tp2Grupo11Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo11Application.class, args);
+	}
+
+}
---
+++ TP2_Grupo11/src/test/java/fi/unju/edu/ar/aplicacion/Tp2Grupo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2Grupo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
